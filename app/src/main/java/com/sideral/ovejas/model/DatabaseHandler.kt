97a48bf0cab56176e264a3e3742311d7159cecb3
Aldo Abril <aldoabril@gmail.com>
2019-10-09 06:09:34 -0500

    CREACION DE TABLAS

--- app/src/main/java/com/sideral/ovejas/model/DatabaseHandler.kt
+++ app/src/main/java/com/sideral/ovejas/model/DatabaseHandler.kt
@@ -18,6 +18,25 @@ class DatabaseHandler(context: Context, factory: SQLiteDatabase.CursorFactory?):
     companion object{
         private  val DB_NAME = "OVEJASDB"
         private  val DB_VERSION = 1
+        //TABLA OVEJAS
+        private val OVEJAS_TABLE_NAME = "OVEJAS"
+        val OVEJAS_ID_COLUMN = "ID"
+        val OVEJAS_PESO_COLUMN = "PESO"
+        val OVEJAS_FECHA_NAC_COLUMN = "FECHA_NACIMIENTO"
+        val OVEJAS_PROPIETARIO_COLUMN = "PROPIETARIO_ID"
+        val OVEJAS_MADRE_COLUMN = "OBJETAS_ID"
+        val OVEJAS_ESTADO = "ESTADO"
+
+        //TABLA PROPIETARIIOS
+        val PROPIETARIOS_TABLE_NAME = "PROPIETARIOS"
+        val PROPIETARIOS_ID_COLUMN = "ID"
+        val PROPIETARIOS_NOMBRE_COLUMN = "NOMBRE"
+
+        //TABLA OVEJAS MOVIMIENTOS
+        val MOVIMIENTOS_TABLE_NAME = "MOVIMIENTOS"
+        val MOVIMIENTOS_ID_COLUMN_= "ID"
+        val MOVIMIENTOS_FECHA_COLUMN = "FECHA"
+        val MOVIMIENTOS_DESCRIPCION = "DESCRIPCION"
 
     }
 }
